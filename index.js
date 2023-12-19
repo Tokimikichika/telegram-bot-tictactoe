f5e5b63daad742f3b0b0bdacec8d8e604068ffca
Tokimikichika <86572389+Tokimikichika@users.noreply.github.com>
2023-12-19 20:40:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramApi = require('node-telegram-bot-api')
 
-const token = '6362367021:AAEVwbVsChw5IwF7EFGeGDwfNcL-dlz-TPo'
+const token = '6362367021:AAEVwbVsChw5IwF7EFGeGDwfNcL-dlz-TP'
 
 const bot = new TelegramApi(token, { polling: true });
 
